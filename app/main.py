856bc1f7793e3982ce46da00a64702403da7a37b
Hernan Ciarlo <ciarlohernan@gmail.com>
2025-06-09 20:23:22 -0300

    Remove unused user model import from main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,6 @@ from fastapi import FastAPI
 from app.routers import auth
 
 from app.db.session import engine
-from app.models import user
 from app.db.base import Base
 
 Base.metadata.create_all(bind=engine)
